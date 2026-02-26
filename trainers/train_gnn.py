4bd2990830e1a15273b611b8bb79ef91af6c5d64
andrypur-hub <andry.pur@gmail.com>
2026-02-26 22:42:34 +0700

    Update train_gnn.py

--- trainers/train_gnn.py
+++ trainers/train_gnn.py
@@ -1,1 +1,24 @@
+import torch
+import torch.nn.functional as F
 
+def train_gnn(model, data, train_mask, epochs=50):
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    y = data.y.float()
+
+    pos_weight = (y[train_mask]==0).sum()/(y[train_mask]==1).sum()
+    for epoch in range(epochs):
+        model.train()
+        optimizer.zero_grad()
+        out = model(data)
+
+        loss = F.binary_cross_entropy_with_logits(
+            out[train_mask],
+            y[train_mask],
+            pos_weight=pos_weight
+        )
+
+        loss.backward()
+        optimizer.step()
+
+    return model
