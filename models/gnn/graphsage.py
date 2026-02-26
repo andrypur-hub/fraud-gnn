ba5a38d2ecfed4503d5bd4a6bcf2e1b0d161dcba
andrypur-hub <andry.pur@gmail.com>
2026-02-26 22:43:13 +0700

    Update graphsage.py

--- models/gnn/graphsage.py
+++ models/gnn/graphsage.py
@@ -1,1 +1,17 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import SAGEConv
 
+class GraphSAGE(nn.Module):
+    def __init__(self, in_dim, hidden_dim=128):
+        super().__init__()
+        self.conv1 = SAGEConv(in_dim, hidden_dim)
+        self.conv2 = SAGEConv(hidden_dim, hidden_dim)
+        self.lin = nn.Linear(hidden_dim, 1)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        x = F.relu(self.conv1(x, edge_index))
+        x = F.relu(self.conv2(x, edge_index))
+        return self.lin(x).squeeze()
