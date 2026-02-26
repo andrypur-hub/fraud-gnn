701939232cffb84520c973afed2b0326df950e7b
andrypur-hub <andry.pur@gmail.com>
2026-02-26 22:44:08 +0700

    Update train_xgb.py

--- baselines/train_xgb.py
+++ baselines/train_xgb.py
@@ -1,1 +1,10 @@
+import xgboost as xgb
 
+def train_xgb(X_train, y_train):
+    model = xgb.XGBClassifier(
+        n_estimators=200,
+        max_depth=6,
+        scale_pos_weight=(len(y_train)-y_train.sum())/max(y_train.sum(),1)
+    )
+    model.fit(X_train, y_train)
+    return model
