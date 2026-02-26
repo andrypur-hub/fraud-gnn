389855af161da778f1c9df9ae6dcccff16c56cfc
andrypur-hub <andry.pur@gmail.com>
2026-02-26 23:20:56 +0700

    Update split_time.py

--- data/split_time.py
+++ data/split_time.py
@@ -1,1 +1,16 @@
+import numpy as np
 
+def time_based_split(timestep):
+    """
+    Elliptic dataset time-based split.
+    
+    Train: timestep <= 34
+    Val:   35–40
+    Test:  > 40
+    """
+
+    train_mask = timestep <= 34
+    val_mask   = (timestep > 34) & (timestep <= 40)
+    test_mask  = timestep > 40
+
+    return train_mask, val_mask, test_mask
